4006fc3db368f1d2ba27d82aa3f233708a7e8b84
Jianfeng Tian <jianfeng.tian@gmail.com>
2015-06-22 16:03:25 +0100

    functional solution

--- src/main/scala/BG/Solution.scala
+++ src/main/scala/BG/Solution.scala
@@ -8,23 +8,6 @@ import scala.collection.JavaConversions._
 object Solution {
   def solution(A: Array[Int]): Int = {
     // write your code in Scala 2.10
-
-    var total = 0
-
-    var arr: Array[Int] = Array()
-
-    def isPermutation(C: Array[Int], i: Int): Boolean = {
-      if (i > C.size) true
-      else {
-        if (C.indexOf(i) == -1) false
-        else isPermutation(C, i + 1)
-      }
-    }
-
-    for (a <- A) {
-      arr = arr :+ a
-      if (isPermutation(arr, 1)) total += 1
-    }
-    total
+    A.size - A.foldLeft(0)((acc, a) => if (a > A.indexOf(a) + 1) acc + 1 else acc)
   }
 }
