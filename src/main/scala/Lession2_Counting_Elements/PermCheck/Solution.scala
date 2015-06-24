5e8c86b84e465a78884096297043a305f2b0e446
Jianfeng Tian <jianfeng.tian@gmail.com>
2015-06-24 13:01:49 +0100

    40% performance

--- src/main/scala/Lession2_Counting_Elements/PermCheck/Solution.scala
+++ src/main/scala/Lession2_Counting_Elements/PermCheck/Solution.scala
@@ -1,27 +1,19 @@
 package Lession2_Counting_Elements.PermCheck
 
-import scala.util.control.Breaks
-
 /**
  * Created by jianfeng on 16/06/2015.
  */
 object Solution {
 
   def solution(A: Array[Int]): Int = {
-    val loop = new Breaks
-    val size = A.length
-    var r = 1
-    var i = 0
-    loop.breakable {
-      for (a <- A) {
-        if (a > size || (A.indexOf(a, i+ 1) != -1)) {
-          r = 0
-          loop.break()
-        }
-        i += 1
+
+    def loop(j: Int): Int = {
+      if (j > A.size) 1
+      else {
+        if (A.indexOf((j)) == -1) 0
+        else loop(j + 1)
       }
     }
-    r
+    loop(1)
   }
-
 }
