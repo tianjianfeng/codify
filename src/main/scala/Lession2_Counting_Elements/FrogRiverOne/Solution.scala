ed2394a7087690cb28695e5420a3a24c238db4f8
Jianfeng Tian <jianfeng.tian@gmail.com>
2015-06-24 13:34:10 +0100

    66% performance

--- src/main/scala/Lession2_Counting_Elements/FrogRiverOne/Solution.scala
+++ src/main/scala/Lession2_Counting_Elements/FrogRiverOne/Solution.scala
@@ -1,26 +1,17 @@
 package Lession2_Counting_Elements.FrogRiverOne
 
-import scala.collection.JavaConversions._
-
 // you can use println for debugging purposes, e.g.
 // println("this is a debug message")
 
 object Solution {
   def solution(X: Int, A: Array[Int]): Int = {
 
-    var t = 0
-
-    def loop(j: Int): Int = {
-      if (j > X) t
-      else {
-        val b = A.indexOf(j)
-        if (b == -1) -1
-        else {
-          if (b > t) t = b
-          loop(j + 1)
-        }
-      }
+    def loop(j: Int, arr: Array[Int]): Array[Int] = {
+      if (j > X) arr
+      else loop(j + 1, arr.+:(A.indexOf(j)))
     }
-    loop(1)
+    val steps = loop(1, Array[Int]())
+
+    if (steps.indexOf(-1) != -1) -1 else steps.max
   }
 }
