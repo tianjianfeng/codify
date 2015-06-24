8f5da1396dc5bc0fba06f3251f6cdc97b724888a
Jianfeng Tian <jianfeng.tian@gmail.com>
2015-06-24 14:56:07 +0100

    100% performance with functional style

--- src/main/scala/Lession3_Prefix_Sums/PassingCars/Solution.scala
+++ src/main/scala/Lession3_Prefix_Sums/PassingCars/Solution.scala
@@ -6,15 +6,14 @@ package Lession3_Prefix_Sums.PassingCars
 object Solution {
   def solution(A: Array[Int]): Int = {
 
-    def loop(j: Int, arr: Array[Int], acc: Int, oneCount: Int): Int = {
+    def loop(j: Int, acc: Int, oneCount: Int): Int = {
       if (acc > 1000000000) -1
       else if (j == A.size - 1 || j == 100000 - 1) acc
       else {
-        if (arr(0) == 0) loop(j + 1, arr.tail, acc + oneCount, oneCount)
-        else loop(j + 1, arr.tail, acc, oneCount -1)
+        if (A(j) == 0) loop(j + 1, acc + oneCount, oneCount)
+        else loop(j + 1, acc, oneCount -1)
       }
     }
-    loop(0, A, 0, A.count(_ == 1))
-
+    loop(0, 0, A.count(_ == 1))
   }
 }
