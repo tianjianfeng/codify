9f7a517a7b06cbfa9b3f1c3368d942e7ee7aa3f9
Jianfeng Tian <jianfeng.tian@gmail.com>
2015-06-24 14:21:32 +0100

    20% performance, functional style

--- src/main/scala/Lession3_Prefix_Sums/PassingCars/Solution.scala
+++ src/main/scala/Lession3_Prefix_Sums/PassingCars/Solution.scala
@@ -1,27 +1,37 @@
 package Lession3_Prefix_Sums.PassingCars
 
-import scala.util.control.Breaks
-
 /**
  * Created by jianfeng on 16/06/2015.
  */
 object Solution {
   def solution(A: Array[Int]): Int = {
 
-    var sum = 0
-
-    def loop(B: Array[Int]): Int = {
-      if (sum > 1000000000) -1
+    def loop(j: Int, arr: Array[Int], acc: Int): Int = {
+      if (acc > 1000000000) -1
+      else if (j == A.size - 1 || j == 100000 - 1) acc
       else {
-        val i = B.indexOf(0)
-        if (i == -1) sum
-        else {
-          val subArr = B.drop(i + 1)
-          sum += subArr.count(_ == 1)
-          loop(subArr)
-        }
+        if (arr(0) == 0) loop(j + 1, arr.drop(1), acc + arr.count(_ == 1))
+        else loop(j + 1, arr.drop(1), acc)
       }
     }
-    loop(A)
+    loop(0, A, 0)
+
+
+
+    //    var sum = 0
+    //
+    //    def loop(B: Array[Int]): Int = {
+    //      if (sum > 1000000000) -1
+    //      else {
+    //        val i = B.indexOf(0)
+    //        if (i == -1) sum
+    //        else {
+    //          val subArr = B.drop(i + 1)
+    //          sum += subArr.count(_ == 1)
+    //          loop(subArr)
+    //        }
+    //      }
+    //    }
+    //    loop(A)
   }
 }
