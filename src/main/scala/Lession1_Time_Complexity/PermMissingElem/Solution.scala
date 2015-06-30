ea118adc4391d5ae89d57b417cd88fe6c871e521
Jianfeng Tian <jianfeng.tian@gmail.com>
2015-06-30 19:13:19 +0100

    tidy up

--- src/main/scala/Lession1_Time_Complexity/PermMissingElem/Solution.scala
+++ src/main/scala/Lession1_Time_Complexity/PermMissingElem/Solution.scala
@@ -1,8 +1,5 @@
 package Lession1_Time_Complexity.PermMissingElem
 
-import scala.collection.JavaConversions._
-import scala.util.control._
-
 
 // you can use println for debugging purposes, e.g.
 // println("this is a debug message")
@@ -10,9 +7,9 @@ import scala.util.control._
 object Solution {
   def solution(A: Array[Int]): Int = {
 
-    def loop (i: Int): Int = {
+    def loop(i: Int): Int = {
       if (A.indexOf(i) == -1) i
-      else loop(i+1)
+      else loop(i + 1)
     }
     loop(1)
 
