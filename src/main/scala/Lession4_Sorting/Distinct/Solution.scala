8998b1af257200afa0538c7bfacac421a140d0ee
Jianfeng Tian <jianfeng.tian@gmail.com>
2015-06-30 15:14:56 +0100

    %33 performance

--- src/main/scala/Lession4_Sorting/Distinct/Solution.scala
+++ src/main/scala/Lession4_Sorting/Distinct/Solution.scala
@@ -8,14 +8,21 @@ import scala.collection.JavaConversions._
 object Solution {
   def solution(A: Array[Int]): Int = {
 
-    def loop(a: Int, acc: Int): Int = {
-      if (a <= 1000000) {
-        if (A.indexOf(a) != -1) loop(a + 1, acc + 1)
-        else loop(a + 1, acc)
-      }
-      else acc
-    }
+//    def loop(a: Int, acc: Int): Int = {
+//      if (a <= 1000000) {
+//        if (A.indexOf(a) != -1) loop(a + 1, acc + 1)
+//        else loop(a + 1, acc)
+//      }
+//      else acc
+//    }
+//
+//    loop(-1000000, 0)
 
-    loop(-1000000, 0)
+    def loop(B: Array[Int], acc: Int): Int = {
+      if (B.isEmpty) acc
+      else loop(B.filterNot(_ == B(0)), acc + 1)
+
+    }
+    loop(A, 0)
   }
 }
